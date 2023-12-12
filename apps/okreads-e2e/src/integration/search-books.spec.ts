5b8c634c89c6e256ea7c697faa27cffd9c3b6059
“ASharma123” <Ankita.Sharma123@t-mobile.com>
2023-12-13 02:00:30 +0530

    Task 2: Adding instant search

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -1,17 +1,7 @@
-describe('When: Use the search feature', () => {
-  beforeEach(() => {
-    cy.startAt('/');
-  });
-
-  it('Then: I should be able to search books by title', () => {
-    cy.get('input[type="search"]').type('javascript');
-
-    cy.get('form').submit();
-
-    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
-  });
-
-  xit('Then: I should see search results as I am typing', () => {
-    // TODO: Implement this test!
+describe("When: Use the search feature", () => {
+  it("Then: I should see search results as I am typing", () => {
+    cy.startAt("/");
+    cy.get('input[type="search"]').type("study");
+    cy.get('[data-testing="book-item"]').should("have.length.greaterThan", 1);
   });
 });
